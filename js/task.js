cecb373ef8c44846034cc86a8dc742846cc56241
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-07-26 21:06:23 +0200

    Added blocking the reload after clicking the event.preventDefault button The task is stored in an array and then displayed on the page. The displayTasks function is responsible for this.

--- js/task.js
+++ js/task.js
@@ -1,12 +1,30 @@
 const tasks = []
 
-const addTask = () => {
+const addTask = (e) => {
+    e.preventDefault()
+
     const task = document.querySelector('#taskInputName')
 
     if (task.value.trim() === '') {
         return alert('Nazwa zadania nie może być pusta.')
     }
 
+    tasks.push(task.value.trim())
+    console.log(tasks)
+    task.value = ''
+    displayTasks()
+}
+
+const displayTasks = () => {
+    const taskList = document.querySelector('#taskList')
+    console.log(taskList)
+    taskList.innerHTML = ''
+
+    for (let i = 0; i < tasks.length; i++) {
+        const listItem = document.createElement('li')
+        listItem.textContent = tasks[i]
+        taskList.appendChild(listItem)
+    }
 }
 
 const addTaskBtn = document.querySelector('#taskAddButton')
