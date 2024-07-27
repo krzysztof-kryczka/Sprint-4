99c00474dd50725fcbe5308df4408960d02cc55f
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-07-28 00:03:51 +0200

    Code optimization

--- js/task.js
+++ js/task.js
@@ -1,8 +1,6 @@
 const tasks = []
 
-const validateTask = (task) => {
-    return task.trim() !== ''
-}
+const validateTask = (task) => task.trim() !== ''
 
 const addTask = (e) => {
     e.preventDefault()
@@ -21,27 +19,19 @@ const addTask = (e) => {
 const editOrDeleteTask = (i, isEdit) => {
     const task = tasks[i]
     const taskList = document.querySelector('#taskList')
-    const existingListItem = taskList.querySelector(
-        'li:nth-child(' + (i + 1) + ')'
-    )
+    const existingListItem = taskList.querySelector('li:nth-child(' + (i + 1) + ')')
 
     if (isEdit) {
         const editInput = document.createElement('input')
         editInput.value = task
-
         const confirmButton = createButton('Zatwierdź zmiany', () => {
             if (!validateTask(editInput.value)) {
                 return alert('Nazwa zadania nie może być pusta.')
             }
-            if (tasks.includes(editInput.value.trim())) {
-                return alert('To zadanie już istnieje na liście.')
-            }
             tasks[i] = editInput.value
             displayTasks()
         })
-        const deleteButton = createButton('Usuń', () => {
-            editOrDeleteTask(i, false)
-        })
+        const deleteButton = createButton('Usuń', () => editOrDeleteTask(i, false))
         existingListItem.innerHTML = ''
         existingListItem.appendChild(editInput)
         existingListItem.appendChild(confirmButton)
@@ -62,12 +52,8 @@ const createButton = (text, clickHandler) => {
 const createListItem = (taskName, i) => {
     const listItem = document.createElement('li')
     listItem.textContent = taskName
-    const taskEditButton = createButton('Edytuj', () => {
-        editOrDeleteTask(i, true)
-    })
-    const taskDeleteButton = createButton('Usuń', () => {
-        editOrDeleteTask(i, false)
-    })
+    const taskEditButton = createButton('Edytuj', () => editOrDeleteTask(i, true))
+    const taskDeleteButton = createButton('Usuń', () => editOrDeleteTask(i, false))
     listItem.appendChild(taskEditButton)
     listItem.appendChild(taskDeleteButton)
     return listItem
